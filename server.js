46539110986c7613d81ce02e7ca1c55fa72796a9
kii-rin <51054640+kii-rin@users.noreply.github.com>
2026-07-02 19:30:27 -0500

    load runner from base64 chunks

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const code = Buffer.from(fs.readFileSync(path.join(__dirname, 'runner.b64'), 'utf8'), 'base64').toString('utf8');
+const files = fs.readdirSync(__dirname).filter((f) => /^runner\.b64\.\d+$/.test(f)).sort();
+if (files.length === 0) throw new Error('Missing runner.b64.xx chunks');
+const code = Buffer.from(files.map((f) => fs.readFileSync(path.join(__dirname, f), 'utf8')).join(''), 'base64').toString('utf8');
 eval(code);
